b47a11f150c1578a8973e86e973ac3a65a688354
Kazumi Inada <hello@nandenjin.com>
2024-02-03 15:15:37 +0900

    docs: Add comments for lib/tile.ts

--- src/lib/tile.ts
+++ src/lib/tile.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts latitude and longitude coordinates to meters using the Web Mercator projection.
+ * @param lat - The latitude coordinate.
+ * @param lon - The longitude coordinate.
+ * @returns An array containing the x and y coordinates in meters.
+ */
 const getMetersByLatLng = function (lat: number, lon: number) {
   const x = (lon * 20037508.34) / 180.0
   let y =
@@ -6,17 +12,32 @@ const getMetersByLatLng = function (lat: number, lon: number) {
   return [x, y]
 }
 
+/** Radius of the Earth in meters */
 const GEO_R = 6378137
-const orgX = -1 * ((2 * GEO_R * Math.PI) / 2)
-const orgY = (2 * GEO_R * Math.PI) / 2
 
-/** 緯度経度とズームレベルからタイルインデックスを取得 */
+/**
+ * The x-coordinate of the origin point.
+ */
+const originX = -1 * ((2 * GEO_R * Math.PI) / 2)
+
+/**
+ * The y-coordinate of the origin point.
+ */
+const originY = (2 * GEO_R * Math.PI) / 2
+
+/**
+ * Calculates the tile coordinates (xtile, ytile) based on the given latitude, longitude, and zoom level.
+ * @param lat - The latitude value.
+ * @param lng - The longitude value.
+ * @param zoom - The zoom level.
+ * @returns An array containing the xtile and ytile values.
+ */
 export const getTileByLatLng = (lat: number, lng: number, zoom: number) => {
   const xy = getMetersByLatLng(lat, lng)
   const unit = (2 * GEO_R * Math.PI) / Math.pow(2, zoom)
 
-  const xtile = Math.floor((xy[0] - orgX) / unit)
-  const ytile = Math.floor((orgY - xy[1]) / unit)
+  const xtile = Math.floor((xy[0] - originX) / unit)
+  const ytile = Math.floor((originY - xy[1]) / unit)
 
   return [xtile, ytile]
 }
